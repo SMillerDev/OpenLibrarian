669fa2b1e626896ea71a2d4a5f4ef5595e3938ae
Sean Molenaar <sean@seanmolenaar.eu>
2023-06-14 20:10:48 +0200

    Initial Commit

---
+++ OpenLibrarian/OpenLibrarianApp.swift
@@ -0,0 +1,20 @@
+//
+//  OpenLibrarianApp.swift
+//  OpenLibrarian
+//
+//  Created by Sean Molenaar on 14/06/2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OpenLibrarianApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
